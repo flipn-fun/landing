938bb5aced70fc2ec95cbb2fe50893f7461a6bef
ben <benjm240@gmail.com>
2025-03-04 19:48:34 +0800

    add x

--- app/sections/landing/index.tsx
+++ app/sections/landing/index.tsx
@@ -1,9 +1,10 @@
 import { motion } from 'framer-motion';
 import styles from './Landing.module.css';
 import { useAccount } from '@/app/useAccount';
+import { WalletModalButton } from '@/app/libs/solana/wallet-adapter/modal';
 
 const Landing = () => {
-    const { connect, address } = useAccount();
+    const { connect, address, connected, disconnect } = useAccount();
 
     //@ts-ignore
     const win: any = typeof window !== "undefined" ? window : {};
@@ -48,12 +49,36 @@ const Landing = () => {
                         >
                             <p className={styles.heroDesc}>Trade and launch memecoins as easy as scrolling Tiktok. Coming soon to Solana.</p>
                         </motion.div>
-                        <button className={styles.watchButton} onClick={() => {
-                            if (!address) {
-                                connect();
-                            }
-                        }}>Connect Wallet</button>
-                        <div className={styles.waitlist}>Connect your wallet and join the waitlist!</div>
+
+                        {
+                            connected ? <div className={styles.connectedWrapper}>
+                                <div className={styles.connected}>
+                                    <div className={styles.connectedAddress}>{ address?.slice(0, 5) + '...' + address?.slice(-5) }</div>
+                                    <div className={styles.connectedIcon} onClick={() => {
+                                        disconnect();
+                                    }}>
+                                        <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                            <path fill-rule="evenodd" clip-rule="evenodd" d="M1.91613 16H10.5731C11.0656 16 11.4652 15.57 11.4652 15.04C11.4652 14.51 11.0656 14.08 10.5731 14.08H2.00906C1.92728 13.974 1.78417 13.662 1.78417 13.164V2.838C1.78417 2.34 1.92728 2.028 2.00906 1.92H10.5731C11.0656 1.92 11.4652 1.49 11.4652 0.96C11.4652 0.43 11.0656 0 10.5731 0H1.91613C0.823322 0 0 1.22 0 2.838V13.162C0 14.78 0.823322 16 1.91613 16ZM12.3929 12.2771L15.7266 8.69156L15.7383 8.67941C15.913 8.49136 16.0004 8.24579 16.0003 8.00023C16.0003 7.75467 15.913 7.5091 15.7383 7.32106L15.7237 7.30575L12.3948 3.72341C12.0454 3.34741 11.4823 3.34741 11.1329 3.72341C10.7835 4.09941 10.7835 4.70541 11.1329 5.08141L12.953 7.03906H6.83918C6.34667 7.03906 5.94709 7.46906 5.94709 7.99906C5.94709 8.52906 6.34667 8.95906 6.83918 8.95906H12.9542L11.1329 10.9191C10.7835 11.2951 10.7835 11.9011 11.1329 12.2771C11.3057 12.4651 11.5343 12.5591 11.7629 12.5591C11.9915 12.5591 12.2201 12.4651 12.3929 12.2771Z" fill="#FF00D9" />
+                                        </svg>
+                                    </div>
+                                </div>
+                                <button className={styles.followButton} onClick={() => {
+                                    window.open(`https://twitter.com/i/oauth2/authorize?response_type=code&client_id=NWZlaG93WlNfNW4xVmxNZHdvUVo6MTpjaQ&redirect_uri=${window.location.href}&scope=tweet.read%20users.read%20follows.read%20like.read&state=state&code_challenge=challenge&code_challenge_method=plain`, '_blank');
+                                }}>Follow X and Join Waitlist</button>
+                            </div> : <>
+                                <WalletModalButton
+                                    style={{
+                                        background: "#FFD700",
+                                        borderRadius: 12,
+                                        padding: "0 10px",
+                                        marginTop: 60
+                                    }}
+                                >
+                                    Connect Wallet
+                                </WalletModalButton>
+                                <div className={styles.waitlist}>Connect your wallet and join the waitlist!</div>
+                            </>
+                        }
 
                         <div className={styles.light1}>
                             <img src="/img/landing/light1.svg" alt="Light 1" />
